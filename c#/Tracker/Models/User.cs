8221695f7e7d24be7f31c4540078c932fbb6c866
Utilisateur <2131793@etu.cegepjonquiere.ca>
2024-12-27 10:17:40 -0500

    razor

---
+++ c#/Tracker/Models/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tracker.Models
+{
+    public class User
+    {
+        public int Email {get;set;}
+        public string? Language {get;set;}
+    }
+}
